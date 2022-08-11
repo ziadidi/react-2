ec3329c278bfdc7c01a083525f8650ca7c0b4365
ziadidi <dianepucci19@gmail.com>
2022-08-11 15:04:31 +0100

    react

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main>
+            <p>I am going to skate with my Boyfriend today!</p>
+        </main>
+    )
+}
+
+export default MainContent
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>AS/DP</h1>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
