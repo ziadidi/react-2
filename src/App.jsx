426508494d0521b297120662845e9d0b5dcb82cf
ziadidi <dianepucci19@gmail.com>
2022-08-12 18:10:37 +0100

    react

--- src/App.jsx
+++ src/App.jsx
@@ -16,30 +16,16 @@
 // export default App;
 
 import React from "react";
-import "./Style.css"
+import TodoItem from "./components/TodoItem";
+import "./Style.css";
 
 function App() {
   return (
-    <div>
-      <h1>To Do List</h1>
-      <div className="todoList">
-        <div className="items">
-          <input id="item1" type="checkbox" checked />
-          <label for="item1">Create a to-do list</label>
-
-          <input id="item2" type="checkbox" />
-          <label for="item2">Take down Christmas tree</label>
-
-          <input id="item3" type="checkbox" />
-          <label for="item3">Learn Ember.js</label>
-
-          <input id="item4" type="checkbox" />
-          <label for="item4">Binge watch every episode of MacGyver</label>
-
-          <input id="item5" type="checkbox" />
-          <label for="item5">Alphabetize everything in the fridge</label>
-        </div>
-      </div>
+    <div className="todo-list">
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,76 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.render(<App/>, document.getElementById("root"))
+
 // import React from "react";
 // import ReactDOM from "react-dom";
-// import App from "./App.jsx";
+// import App from "./App";
+
+// ReactDOM.render(<App/>, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// function App() {
+//     const firstName ="Diane"
+//     const lastName = "Pucci"
+
+//     return (
+//         <h1>Hello {`${firstName} ${lastName}`}!</h1>
+//     )
+// }
 
 // ReactDOM.render(<App/>, document.getElementById("root"))
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+
+//  import React from "react";
+//  import ReactDOM from "react-dom";
+
+//  function App() {
+//      const date = new Date()
+//      const hours = date.getHours()
+//      let timeofDay
+//      const styles = {
+//         fontSize: 30
+        
+//      }
+
+//      if (hours < 12) {
+//          timeofDay = "morning"
+//          styles.color = "#FFF933"
+//          styles.backgroundColor = "#2A8A22"
+//      } else if (hours >= 12 && hours < 17) {
+//          timeofDay = "afternoon"
+//          styles.color = "#FF33A7"
+//          styles.backgroundColor = "#0F2C7C"
+//      } else {
+//          timeofDay = "night"
+//          styles.color = "#3371FF"
+//          styles.backgroundColor = "#4C4E52"
+//      }
+
+
+//      return (
+//          <h1 style = {styles}> Good {timeofDay}!</h1>
+//      )
+//  }
+
+//  ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// function App() {
+//     const date = new Date()
+
+//     return (
+//         <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
+//     )
+// }
+
+// ReactDOM.render(<App/>, document.getElementById("root"))
