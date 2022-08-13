1710cffe6f75a36eae35ab46685489f884062e40
ziadidi <dianepucci19@gmail.com>
2022-08-13 18:48:13 +0100

    react

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,46 @@
 
 // export default App;
 
+// import React from "react";
+// import TodoItem from "./components/TodoItem";
+// import "./Style.css";
+
+// function App() {
+//   return (
+//     <div className="todo-list">
+//         <TodoItem/>
+//         <TodoItem/>
+//         <TodoItem/>
+//         <TodoItem/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import TodoItem from "./components/TodoItem";
-import "./Style.css";
+import ContactApp from "./ContactApp";
+import Style from "./Style.css"
 
 function App() {
   return (
-    <div className="todo-list">
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+    <div className="contacts">
+      <ContactApp
+      contact = {{name: "Mmm...", imgUrl: "https://imgflip.com/s/meme/Smiling-Cat.jpg",
+        phone: "3287546475", email: "mmm@mlml.fur"}}
+      />
+      <ContactApp
+        contact = {{name: "Mmm...", imgUrl: "https://imgflip.com/s/meme/Smiling-Cat.jpg",
+        phone: "3287546475", email: "mmm@mlml.fur"}}
+      />
+      <ContactApp
+        contact = {{name: "Mmm...", imgUrl: "https://imgflip.com/s/meme/Smiling-Cat.jpg",
+        phone: "3287546475", email: "mmm@mlml.fur"}}
+      />
+      <ContactApp
+        contact = {{name: "Mmm...", imgUrl: "https://imgflip.com/s/meme/Smiling-Cat.jpg",
+        phone: "3287546475", email: "mmm@mlml.fur"}}
+      />
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
 
-function TodoItem() {
-    return (
-        <div className="todo-item">
-        <input type= "checkbox" />
-        <p>i love ancinio</p>
-        </div>
-    )
-}
+// function TodoItem() {
+//     return (
+//         <div className="todo-item">
+//         <input type= "checkbox" />
+//         <p>i love ancinio</p>
+//         </div>
+//     )
+// }
 
-export default TodoItem
+// export default TodoItem
